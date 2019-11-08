55b207f5948b66607b13b0ea605ca44faf6a53a3
AndrewZhang <zzq.happy@qq.com>
2019-11-08 15:02:15 +0800

    首页底部导航栏制作

--- android/app/src/main/java/com/betterzhang/flutter_trip_demo/MainActivity.java
+++ android/app/src/main/java/com/betterzhang/flutter_trip_demo/MainActivity.java
@@ -2,11 +2,13 @@ package com.betterzhang.flutter_trip_demo;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import org.devio.flutter.splashscreen.flutter_splash_screen.SplashScreen;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
   }
