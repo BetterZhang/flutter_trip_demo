c9331df047c1669d5873f1518fb894ca06f5ce6c
AndrewZhang <zzq.happy@qq.com>
2019-11-11 16:03:04 +0800

    旅拍Tab页面

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,17 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trip_demo/model/travel_tab_model.dart';
+import 'package:flutter_trip_demo/dao/travel_tab_dao.dart';
+import 'package:flutter_trip_demo/pages/travel_tab_page.dart';
 
-
+// 旅拍页面
 class TravelPage extends StatefulWidget {
   @override
   _TravelPageState createState() => _TravelPageState();
 }
 
-class _TravelPageState extends State<TravelPage> {
+class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
+
+  TabController _controller;
+  List<TravelTab> tabs = [];
+  TravelTabModel travelTabModel;
+
+  @override
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // 初始化tab数据
+  void _loadData() async {
+    _controller = TabController(length: 0, vsync: this);
+    try {
+      TravelTabModel model = await TravelTabDao.fetch();
+      _controller = TabController(length: model.tabs.length, vsync: this);
+      setState(() {
+        tabs = model.tabs;
+        travelTabModel = model;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        '旅拍'
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.only(top: 30),
+            child: TabBar(
+              controller: _controller,
+              isScrollable: true,
+              labelColor: Colors.black,
+              labelPadding: EdgeInsets.fromLTRB(20, 0, 20, 5),
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+                  color: Color(0xff1fcfbb),
+                  width: 3
+                ),
+                insets: EdgeInsets.only(bottom: 10),
+              ),
+              tabs: tabs.map<Tab>((TravelTab tab) {
+                return Tab(
+                  text: tab.labelName,
+                );
+              }).toList(),
+            ),
+          ),
+          Flexible(
+            child: TabBarView(
+              controller: _controller,
+              children: tabs.map((TravelTab tab) {
+                return TravelTabPage(
+
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/pages/travel_tab_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TravelTabPage extends StatefulWidget {
+  @override
+  _TravelTabPageState createState() => _TravelTabPageState();
+}
+
+class _TravelTabPageState extends State<TravelTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Travel Tab Page'),
+      ),
+    );
+  }
+}
